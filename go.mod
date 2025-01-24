7d476a8a0d744928e780f56b0269a9855a3953af
pryzmatyk <you@example.com>
2025-01-24 00:00:46 -0700

    add mor initial stuff

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module revenant
 
 go 1.22.10
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/websocket v1.5.3
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/lib/pq v1.10.9
+)
---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/websocket"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+var (
+	db        *sqlx.DB
+	upgrader  = websocket.Upgrader{}
+	jwtSecret = []byte("your_secret_key") // Change this to a secure key
+)
+
+type User struct {
+	ID       int    `db:"id" json:"id"`
+	Username string `db:"username" json:"username"`
+	Password string `db:"password" json:"-"`
+}
+
+type Message struct {
+	User    string `json:"user"`
+	Content string `json:"content"`
+}
+
+// Initialize the database connection
+func initDB() {
+	var err error
+	dsn := "user=youruser password=yourpassword dbname=yourdb host=db port=5431 sslmode=disable" // Update with your DB credentials
+	db, err = sqlx.Connect("postgres", dsn)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// JWT Middleware
+func jwtMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenString := r.Header.Get("Authorization")
+		if tokenString == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+// WebSocket handler
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Error upgrading connection:", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		var msg Message
+		err := conn.ReadJSON(&msg)
+		if err != nil {
+			log.Println("Error reading JSON:", err)
+			break
+		}
+		log.Printf("Received message: %+v\n", msg)
+		// Echo the message back
+		err = conn.WriteJSON(msg)
+		if err != nil {
+			log.Println("Error writing JSON:", err)
+			break
+		}
+	}
+}
+
+// RESTful API to get users
+func getUsersHandler(w http.ResponseWriter, r *http.Request) {
+	var users []User
+	err := db.Select(&users, "SELECT id, username FROM users")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+// Main function
+func main() {
+	initDB()
+	defer db.Close()
+
+	http.HandleFunc("/ws", wsHandler)
+	http.Handle("/api/users", jwtMiddleware(http.HandlerFunc(getUsersHandler)))
+
+	// Serve static files (HTML, CSS)
+	http.Handle("/", http.FileServer(http.Dir("./static"))) // Place your HTML and CSS in the static directory
+
+	fmt.Println("Server started at :8081")
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
